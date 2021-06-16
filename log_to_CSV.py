6bc5c8bfad648bf24ead8423f807dfb1008d0d0f
polanabil85 <61789283+polanabil85@users.noreply.github.com>
2021-06-16 16:48:56 +0200

    Add files via upload

---
+++ log_to_CSV.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import numpy as np
+import re
+nan = np.nan
+rows=[]
+dict1={}
+with open('zten31_e300_Auto_Long.log','r') as reader:
+    for line in reader:
+        if 'Event Processor' in line:
+            if '[Event Processor] Processing alert' in line:
+                continue
+            strin1 = line.split(']')
+            strin2 = strin1[1].split(':')
+            dict1[strin2[0].strip()]=' '.join(strin2[1:]).strip()
+        if 'Processing Alert...' in line:
+            rows.append(dict1)
+datafr=pd.DataFrame.from_dict(rows, orient='columns')
+datafr.to_csv('alarms.csv')
